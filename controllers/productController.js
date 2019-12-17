f8c664baf0e74b512463f8f342561d7f81a1f856
kevin <keebvhev@gmail.com>
2019-12-17 18:24:10 +0800

    F4 products pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Product = db.Product
 const Category = db.Category
+const pageLimit = 10
+
 
 
 const productController = {
@@ -9,17 +11,34 @@ const productController = {
 
     // Product.findAll({ include: Category }).then(products => {
 
+    let offset = 0
     let whereQuery = {}
     let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery['CategoryId'] = categoryId
     }
 
-    Product.findAll({ include: Category, where: whereQuery }).then(products => {
+    // Product.findAll({ include: Category, where: whereQuery }).then(products => {
 
 
-      const data = products.map(r => ({
+    //   const data = products.map(r => ({
+
+
+    Product.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit }).then(result => {
+      // data for pagination
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
+      // clean up restaurant data
+      const data = result.rows.map(r => ({
+
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50)
       }))
@@ -27,7 +46,11 @@ const productController = {
         return res.render('index', {
           products: data,
           categories: categories,
-          categoryId: categoryId
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
         })
 
       })
