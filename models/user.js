b25283e322b4b3f107706ea186163e9a5b9973c0
kevin <keebvhev@gmail.com>
2020-01-17 20:21:48 +0800

    add user.role default is user

--- models/user.js
+++ models/user.js
@@ -3,13 +3,16 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    role: DataTypes.STRING,
+    role: {
+      type: DataTypes.STRING,
+      defaultValue: 'user'
+    },
     name: DataTypes.STRING,
     tel: DataTypes.STRING,
     address: DataTypes.STRING,
     image: DataTypes.STRING
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
     // associations can be defined here
     User.hasMany(models.Order)
   };
